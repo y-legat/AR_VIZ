eb13b2374a5208dacbbc044d72f22f7f772d4885
SC-VRDEMO\ylegat <yessika.legat@dlr.de>
2018-05-07 16:20:08 +0200

    highlight parent element, showCity Button method

---
+++ Assets/Scripts/CityScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void showCityButton()
+    {
+        //set main menu inactive
+        GameObject.Find("Menu_main").SetActive(false);
+        //set City active
+        GameObject.Find("SzeneContent/Holograms/City").SetActive(true);
+        //GameObject.Find("SzeneContent/InformationPanel").SetActive(true);
+
+
+    }
+}
--- Assets/Scripts/HoverScript.cs
+++ Assets/Scripts/HoverScript.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 public class HoverScript : MonoBehaviour, IFocusable, IInputClickHandler
 {
     private Material[] defaultMaterials;
+    private Material tmp;
     private GameObject gameobj;
     private Text gameObjName;
     private Text gameObjtype;
@@ -21,6 +22,7 @@ public class HoverScript : MonoBehaviour, IFocusable, IInputClickHandler
         defaultMaterials = GetComponent<Renderer>().materials;
         if (GameObject.Find("InformationPanel") != null)
         {
+            //find text elements, which will be overwritten:
             gameObjName = GameObject.Find("InformationPanel/TextContent/Subtitle01/Subtitle01.1").GetComponent<Text>();
             gameObjtype = GameObject.Find("InformationPanel/TextContent/Subtitle02/Subtitle02.1").GetComponent<Text>();
             gameObjPackage = GameObject.Find("InformationPanel/TextContent/Subtitle03/Subtitle03.1").GetComponent<Text>();
@@ -31,8 +33,6 @@ public class HoverScript : MonoBehaviour, IFocusable, IInputClickHandler
     private void Awake()
     {
         defaultMaterials = GetComponent<Renderer>().materials;
-
-
     }
 
     public void OnFocusEnter()
@@ -40,10 +40,15 @@ public class HoverScript : MonoBehaviour, IFocusable, IInputClickHandler
         for (int i = 0; i < defaultMaterials.Length; i++)
         {
             // Highlight the material when gaze enters using the shader property.
-            defaultMaterials[i].SetFloat("_Gloss", 10.0f);
-
-           
+            defaultMaterials[i].SetFloat("_Gloss", 7.0f);
+            //store original material for OnFocusExit()
+            tmp = gameObject.transform.parent.GetComponent<Renderer>().material;
+            //set Parent (package) to houseColor 
+            gameObject.transform.parent.GetComponent<Renderer>().material = defaultMaterials[i];
+            //gameObject.transform.parent.GetComponent<Renderer>().material.SetFloat("_Gloss", 10.0f);
         }
+        Debug.Log(gameObject.transform.parent.name);
+        
     }
 
     public void OnFocusExit()
@@ -52,6 +57,8 @@ public class HoverScript : MonoBehaviour, IFocusable, IInputClickHandler
         {
             // Remove highlight on material when gaze exits.
             defaultMaterials[i].SetFloat("_Gloss", 1.0f);
+            //reset parent matrial 
+            gameObject.transform.parent.GetComponent<Renderer>().material = tmp;
         }
     }
 
@@ -66,21 +73,12 @@ public class HoverScript : MonoBehaviour, IFocusable, IInputClickHandler
     {
         if (GameObject.Find("InformationPanel") != null)
         {
+            //set Text 
             gameObjName.text = gameObject.name;
             gameObjPackage.text = gameObject.transform.parent.name;
-
             gameObjtype.text = "TODO";
         }
         
-
-
-
     }
 
-    public void helper()
-    {
-        GameObject.Find("InformationPanel").SetActive(true);
-
-        Debug.Log("hi");
-    }
 }
