fbd3716f2753d2c88bb09fae3866dafccdf98d13
SC-VRDEMO\ylegat <yessika.legat@dlr.de>
2018-05-14 19:02:20 +0200

    added Services Prototype

---
+++ Assets/Scripts/ImportExportScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class ImportExportScript : MonoBehaviour, IFocusable, IInputClickHandler
+{
+    private GameObject child;
+    public bool clicked;
+
+    public void OnFocusEnter()
+    {
+        
+ 
+    }
+
+    public void OnFocusExit()
+    {
+        
+        
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (transform.childCount > 0)
+        {
+                
+            child = gameObject.transform.GetChild(0).gameObject;
+
+            if (clicked && child != null)
+            {
+                child.SetActive(false);
+                clicked = false; 
+            
+            }
+            else if(child != null)
+            {
+                child.SetActive(true);
+                clicked = true;
+            }
+
+        }
+
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        clicked = false; 
+        
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
