49c9fc92a035f2cac668406e526f60cffa885725
SC-VRDEMO\ylegat <yessika.legat@dlr.de>
2018-05-16 13:29:03 +0200

    ImportExport Script

--- Assets/Scripts/ImportExportScript.cs
+++ Assets/Scripts/ImportExportScript.cs
@@ -7,17 +7,32 @@ public class ImportExportScript : MonoBehaviour, IFocusable, IInputClickHandler
 {
     private GameObject child;
     public bool clicked;
+    private Material [] highlightMaterials;
 
     public void OnFocusEnter()
     {
-        
+        if (transform.childCount > 0)
+        {
+
+            child = gameObject.transform.GetChild(0).gameObject;
+            child.SetActive(true);
+            
+            highlightMaterials[0].SetFloat("_Gloss", 5.0f);
+            Debug.Log(highlightMaterials[0].name);
+        }
+            
  
     }
 
     public void OnFocusExit()
     {
-        
-        
+        if (!clicked && child != null)
+        {
+            child.SetActive(false);
+            clicked = false;
+            highlightMaterials[0].SetFloat("_Gloss", 1.0f);
+        }
+
     }
 
     public void OnInputClicked(InputClickedEventData eventData)
@@ -47,10 +62,10 @@ public class ImportExportScript : MonoBehaviour, IFocusable, IInputClickHandler
     // Use this for initialization
     void Start () {
 
-        clicked = false; 
-        
-       
-	}
+        clicked = false;
+        highlightMaterials = GetComponent<Renderer>().materials;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
